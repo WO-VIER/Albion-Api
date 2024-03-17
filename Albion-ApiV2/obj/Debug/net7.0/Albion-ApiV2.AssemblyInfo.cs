3a0e4c404913319d5513ae8129d94b18a8f2474e
60784 <60784@etu.he2b.be>
2024-03-17 16:36:15 +0100

    Correctif bug couleurs

--- Albion-ApiV2/obj/Debug/net7.0/Albion-ApiV2.AssemblyInfo.cs
+++ Albion-ApiV2/obj/Debug/net7.0/Albion-ApiV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Albion-ApiV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23892cfed164e4e8aaa5646861d784f0d43f54fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Albion-ApiV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Albion-ApiV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
